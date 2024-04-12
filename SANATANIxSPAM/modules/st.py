bfd9d690d7b4f64d7baf344a7b0b91f141f71946
It's RAJPUT <160611013+ITS-MY-EDIT@users.noreply.github.com>
2024-04-12 22:58:04 +0530

    Update and rename 4st.py to st.py

--- SANATANIxSPAM/modules/st.py
+++ SANATANIxSPAM/modules/st.py
@@ -2,7 +2,7 @@ import asyncio
 from random import choice
 from telethon import events
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from SANATANIxSPAM.data import 4ST
+from SANATANIxSPAM.data import ST
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%s4st(?: |$)(.*)" % hl))
 @X2.on(events.NewMessage(incoming=True, pattern=r"\%s4st(?: |$)(.*)" % hl))
